180ef327e01046c489d9e761d5508791932ae1a5
AvinashS97 <avikumar97170@gmail.com>
2025-05-22 17:15:13 +0530

    22.2

--- src/components/Layout/AppLayout.jsx
+++ src/components/Layout/AppLayout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom"
-import { Footer } from "../UI/Footer"
 import { Headers } from "../UI/Headers"
+import { Footers } from "../UI/Footer"
 
 export const AppLayout = () => {
     return <>
         <Headers />
         <Outlet />
-        <Footer />
+        <Footers />
     </>
 }
